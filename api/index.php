4f4a58f9c056caa5e2551b676f0af1b2c50ed38e
Adam Field <af05v@soton.ac.uk>
2014-07-23 16:46:14 +0100

    fatfree installed for data API (need flexibility when talking to the flash objects)

---
+++ api/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$f3=require('lib/fatfree/lib/base.php');
+
+$f3->set('DEBUG',1);
+
+$f3->config('config.ini');
+
+
+
+function get_answer($f3)
+{
+	dump_request($f3)
+}
+
+function save_answer($f3)
+{
+	dump_request($f3)
+}
+function session_exists($f3)
+{
+	dump_request($f3)
+}
+function get_all_user_answer($f3)
+{
+	dump_request($f3)
+}
+function clear_answer($f3)
+{
+	dump_request($f3)
+}
+
+function dump_request($f3)
+{
+	error_log(print_r($f3->get('REQUEST')));
+}
+
+
+
+
+
+$f3->run();
