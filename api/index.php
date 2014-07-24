6a1c337653433c352f296b336dd8f1c735c6e6ad
Adam Field <af05v@soton.ac.uk>
2014-07-24 10:50:49 +0100

    correct URL being used in flash args

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 $f3=require('lib/fatfree/lib/base.php');
 
-$f3->set('DEBUG',1);
+$f3->set('DEBUG',3);
 
 $f3->config('config.ini');
 
@@ -10,29 +10,29 @@ $f3->config('config.ini');
 
 function get_answer($f3)
 {
-	dump_request($f3)
+	dump_request($f3);
 }
 
 function save_answer($f3)
 {
-	dump_request($f3)
+	dump_request($f3);
 }
 function session_exists($f3)
 {
-	dump_request($f3)
+	dump_request($f3);
 }
 function get_all_user_answer($f3)
 {
-	dump_request($f3)
+	dump_request($f3);
 }
 function clear_answer($f3)
 {
-	dump_request($f3)
+	dump_request($f3);
 }
 
 function dump_request($f3)
 {
-	error_log(print_r($f3->get('REQUEST')));
+	error_log(print_r($f3->get('REQUEST'),True));
 }
 
 
